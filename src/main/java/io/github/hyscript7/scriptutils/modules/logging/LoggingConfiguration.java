7934046c7940a64fa9a3a0204894784e72f9e873
HyScript7 <hyscript7@seznam.cz>
2025-10-23 19:57:32 +0200

    ✨ Implement member event logging

--- src/main/java/io/github/hyscript7/scriptutils/modules/logging/LoggingConfiguration.java
+++ src/main/java/io/github/hyscript7/scriptutils/modules/logging/LoggingConfiguration.java
@@ -1,8 +1,23 @@
 package io.github.hyscript7.scriptutils.modules.logging;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.hyscript7.scriptutils.modules.logging.api.services.DiscordLoggingService;
+import io.github.hyscript7.scriptutils.modules.logging.internal.listeners.MemberLoggingListener;
 
 @Configuration
 public class LoggingConfiguration {
-    
+    @Bean
+    MemberLoggingListener memberLoggingListener(DiscordLoggingService discordLoggingService) {
+        return new MemberLoggingListener(discordLoggingService);
+    }
+
+    @Bean
+    LoggingModule loggingModule(MemberLoggingListener memberLoggingListener) {
+        LoggingModule loggingModule = new LoggingModule();
+        loggingModule.addEventListener(memberLoggingListener);
+        return loggingModule;
+    }
+
 }
