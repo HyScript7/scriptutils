5a5378936cb43017c20cd2f121875219242372fd
HyScript7 <hyscript7@seznam.cz>
2025-10-23 19:50:39 +0200

    🎨 Define some useful colors in Constants

--- src/main/java/io/github/hyscript7/scriptutils/infrastructure/Constants.java
+++ src/main/java/io/github/hyscript7/scriptutils/infrastructure/Constants.java
@@ -2,9 +2,23 @@ package io.github.hyscript7.scriptutils.infrastructure;
 
 import java.time.Duration;
 
+import lombok.Getter;
+
 public class Constants {
     public static final String DEFAULT_REASON = "No reason specified.";
     public static final Duration DEFAULT_PURGE_DURATION = Duration.ofDays(7L);
+
+    @Getter
+    public enum Colors {
+        RED(0xFF0000),
+        GREEN (0x00FF00),
+        YELLOW(0xFFFF00);
+
+        public final int value;
+        Colors(int value) {
+            this.value = value;
+        }
+    }
 
     private Constants() {}
 }
