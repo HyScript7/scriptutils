fcc5d035a1e9dd0b48c3ff015884d1829c375d22
HyScript7 <hyscript7@seznam.cz>
2025-10-23 19:50:53 +0200

    ✨ Implement log relay / log consumer

---
+++ src/main/java/io/github/hyscript7/scriptutils/modules/logging/api/services/DiscordLoggingService.java
@@ -0,0 +1,50 @@
+package io.github.hyscript7.scriptutils.modules.logging.api.services;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import io.github.hyscript7.scriptutils.modules.logging.api.LogEntry;
+import io.github.hyscript7.scriptutils.modules.logging.api.LogCategory;
+import io.github.hyscript7.scriptutils.modules.logging.internal.models.ServerSettings;
+import io.github.hyscript7.scriptutils.modules.logging.internal.services.ServerSettingsService;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.IncomingWebhookClient;
+import net.dv8tion.jda.api.entities.WebhookClient;
+
+@Service
+public class DiscordLoggingService {
+
+    private final ServerSettingsService serverSettingsService;
+
+    DiscordLoggingService(ServerSettingsService serverSettingsService) {
+        this.serverSettingsService = serverSettingsService;
+    }
+
+    public void log(LogEntry action) {
+        ServerSettings settings = serverSettingsService.getSettingsForGuild(action.guildId());
+        Optional<String> defaultWebhookUrl = Optional.ofNullable(settings.getDefaultWebhookUrl());
+        Optional<String> webhookUrl = Optional.ofNullable(switch (action.category()) {
+            case LogCategory.DEFAULT -> settings.getDefaultWebhookUrl();
+            case LogCategory.MESSAGE -> settings.getMessageWebhookUrl();
+            case LogCategory.MEMBER -> settings.getMemberWebhookUrl();
+            case LogCategory.SERVER -> settings.getServerWebhookUrl();
+            case LogCategory.VOICE -> settings.getVoiceWebhookUrl();
+            case LogCategory.MODERATION -> settings.getModerationWebhookUrl();
+            case LogCategory.AUTO_MODERATION -> settings.getAutoModerationWebhookUrl();
+        });
+
+        if (webhookUrl.isEmpty() && defaultWebhookUrl.isEmpty()) {
+            return;
+        }
+
+        if (webhookUrl.isEmpty()) {
+            webhookUrl = defaultWebhookUrl;
+        }
+
+        IncomingWebhookClient webhookClient = WebhookClient.createClient(action.jda(), webhookUrl.get());
+        webhookClient.sendMessageEmbeds(action.embed()).queue();
+    }
+
+}
